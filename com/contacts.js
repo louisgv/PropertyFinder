376967e490bb7191c159f303abde1ed83cc29480
Louis Vichy <lgvichy@gmail.com>
2015-11-28 11:59:30 -0800

    Next step, React with classes

--- com/contacts.js
+++ com/contacts.js
@@ -1,20 +1,33 @@
+var contacts = [
+  {
+    key: 1,
+    name: "Louis Vichy",
+    email: "louis@me.com"
+  },
+  {
+    key: 2,
+    name: "Alice",
+    email: "alice@me.com"
+  }
+]
+
+var listElements = contacts.filter((contact) => {
+    return contact.email;
+  })
+  .map((contact) => {
+    return React.createElement('li', {
+        key: contact.key
+      },
+      React.createElement('h2', {}, contact.name),
+      React.createElement('a', {
+        href: 'mailto:' + contact.email
+      }, contact.email))
+  })
+
 var rootElement =
   React.createElement('div', {},
     React.createElement('h1', {}, "Contacts"),
-    React.createElement('ul', {},
-      React.createElement('li', {},
-        React.createElement('h2', {}, "Louis Vichy"),
-        React.createElement('a', {
-          href: 'mailto:lgvichy@gmail.com'
-        }, 'lgvichy@gmail.com')
-      ),
-      React.createElement('li', {},
-        React.createElement('h2', {}, "Alan Roster"),
-        React.createElement('a', {
-          href: 'mailto:alan@jabsquared.ninja'
-        }, 'alan@jabsquared.ninja')
-      )
-    )
+    React.createElement('ul', {}, listElements)
   );
 
 ReactDOM.render(rootElement, document.getElementById('react-app'));
