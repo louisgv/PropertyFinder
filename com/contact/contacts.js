8eabf9132afad355014b24877fbbcdcbeae41bd0
Louis Vichy <lgvichy@gmail.com>
2015-11-28 14:56:11 -0800

    Added style, as well as making the form and view. Looking at how to style, generally everything is then put into another class to be render, with class insert right there.

---
+++ com/contact/contacts.js
@@ -0,0 +1,108 @@
+// var ContactView = React.createClass({
+//   propsTypes: {
+//     contacts: React.propsTypes.array.isRequired,
+//     newContact: React.propsTypes.object.isRequired,
+//   },
+//   render: function() {
+//     return (
+//       React.createElement
+//     );
+//   }
+// })
+
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+  render: function () {
+    return(
+      React.createElement('form', {},
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Name',
+          value: this.props.contact.name,
+        }),
+        React.createElement('input', {
+          type: 'email',
+          placeholder: 'Email',
+          value: this.props.contact.email,
+        }),
+        React.createElement('textarea', {
+          placeholder: 'Description',
+          value: this.props.contact.description
+        }),
+        React.createElement('button', {
+          type: 'submit'
+        }, 'submit')
+      )
+    );
+  }
+})
+
+var ContactItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    email: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string
+  },
+
+  render: function () {
+    return(
+      React.createElement('li', {
+          className: 'contact-item'
+        },
+        React.createElement('h2', {
+          className: 'contact-item-name'
+        }, this.props.name),
+        React.createElement('a', {
+          className: 'contact-item-email',
+          href: 'mailto:' + this.props.email
+        }, this.props.email),
+        React.createElement('div', {
+          className: 'contact-item-desc'
+        }, this.props.description)
+      )
+    )
+  }
+})
+
+var contacts = [
+  {
+    key: 1,
+    name: 'Louis Vichy',
+    email: 'louis@me.com',
+    description: 'do everything'
+  },
+  {
+    key: 2,
+    name: 'Alice',
+    email: 'alice@me.com'
+  },
+  {
+    key: 3,
+    name: 'Balista'
+  }
+]
+
+var listElements = contacts.filter((contact) => {
+    return contact.email;
+  })
+  .map((contact) => {
+    return React.createElement(ContactItem, {
+      key: contact.key,
+      name: contact.name,
+      email: contact.email,
+      description: contact.description
+    })
+  })
+
+var rootElement =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Contacts'),
+    React.createElement('ul', {}, listElements),
+    React.createElement(ContactForm, {
+      contact: {}
+    })
+  );
+
+ReactDOM.render(rootElement, document.getElementById('react-app'));
