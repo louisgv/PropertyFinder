9c4b769b0d6e0a9d290051a3ef334a2d16a09214
Louis Vichy <lgvichy@gmail.com>
2015-11-28 12:14:55 -0800

    Done with creating the class, now to apply it

--- com/contacts.js
+++ com/contacts.js
@@ -1,13 +1,42 @@
+var ContactItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+  },
+
+  render: function () {
+    return {
+      React.createElement('li', {
+          className: 'Contact'
+        },
+        React.createElement('h2', {
+          className: 'Contact-name'
+        }, this.props.name),
+        React.createElement('a', {
+          className: 'Contact-email'
+        }, this.props.email),
+        React.createElement('p', {
+          className: 'Contact-description'
+        }, this.props.description)
+      )
+    }
+  }
+})
+
 var contacts = [
   {
     key: 1,
-    name: "Louis Vichy",
-    email: "louis@me.com"
+    name: 'Louis Vichy',
+    email: 'louis@me.com',
+    description: 'do everything'
   },
   {
     key: 2,
-    name: "Alice",
-    email: "alice@me.com"
+    name: 'Alice',
+    email: 'alice@me.com'
+  },
+  {
+    key: 3,
+    name: 'Balista'
   }
 ]
 
@@ -26,7 +55,7 @@ var listElements = contacts.filter((contact) => {
 
 var rootElement =
   React.createElement('div', {},
-    React.createElement('h1', {}, "Contacts"),
+    React.createElement('h1', {}, 'Contacts'),
     React.createElement('ul', {}, listElements)
   );
 
